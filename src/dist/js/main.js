7f1aa15d33b8864d5b8270d42a240d837e3a18d2
Szabolcs Ronyai <szronyai@gmail.com>
2022-05-09 20:29:27 +0200

    dd

--- src/dist/js/main.js
+++ src/dist/js/main.js
@@ -78,3 +78,8 @@ const logo = document.querySelectorAll("#design-logo path");
 for (let i = 0; i < logo.length; i++) {
     console.log(`Letter ${i} is ${logo[i].getTotalLength()}`)
 }
+
+
+mediumZoom('.zoom', {
+    margin: 50
+})
